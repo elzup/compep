6849649fc2578261ab5f7711910377390c558c50
elzup <guild0105@gmail.com>
2017-10-09 14:33:37 +0900

    Change Template to friendly

--- template/main.cpp
+++ template/main.cpp
@@ -1,18 +1,5 @@
 #include <bits/stdc++.h>
-#include <regex>
 using namespace std;
-typedef long long ll;
-
-#define ALL(a) (a.begin()),(a.end())
-#define ZERO(a) memset(a, 0, sizeof(a))
-#define MINUS(a) memset(a,0xff,sizeof(a))
-#define SIZE(array) (sizeof(array) / sizeof(array[0]))
-
-#define FOR(i,a,b) for(int i=(a);i<(b);++i)
-#define REP(i,n)  FOR(i,0,n)
-
-#define MOD 1000000007L
-#define INF 100000000000
 
 signed main() {
 	int a, b;
